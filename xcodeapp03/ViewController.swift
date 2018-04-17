a2a67e882724f66bd1b9e2d6fdc8fc49f91fd68c
stoller <stoller@stollerde-MacBook-Pro.local>
2018-04-17 19:12:46 +0800

    mybearchangecolor_final

--- xcodeapp03/ViewController.swift
+++ xcodeapp03/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+   
+    @IBOutlet weak var bearColorView: UIImageView!
+    @IBOutlet weak var bearView: UIImageView!
+    @IBAction func colorValueChange(_ sender: Any) {
+         bearColorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var redSwitch: UISwitch!
+    
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
+    @IBAction func colorswitch(_ sender: UISwitch) {
+        
+        if sender == redSwitch {
+        
+        if redSwitch.isOn == false {
+            redSlider.isEnabled = false
+        }
+        else {
+            redSlider.isEnabled = true
+        }
+        }
+         else if sender == greenSwitch {
+        
+        if greenSwitch.isOn == false {
+            greenSlider.isEnabled = false
+        }
+            else {
+            greenSlider.isEnabled = true
+        }
+    }
+        else if sender == blueSwitch
+        {
+        if blueSwitch.isOn == false {
+            blueSlider.isEnabled = false
+        }
+        else {
+            blueSlider.isEnabled = true
+        }
 
+    }
+}
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
